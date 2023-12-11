480e7e20d515b5d7b41ac39a72d19c7d19950d0b
Elena Seliveystrova <magicnimfa@gmail.com>
2023-12-11 19:37:52 +0200

    fixed incorrect selectors, added parametrized tests by file, method sources, value sources.

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -3,11 +3,8 @@ import org.hamcrest.CoreMatchers;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.junit.jupiter.params.provider.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -15,18 +12,20 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+
 public class SignInTest {
-    @FindBy(css = "app-header:nth-child(1) .ubs-header-sing-in-img")
+    @FindBy(css = "app-ubs .ubs-header-sing-in-img")
     private WebElement signInButton;
     @FindBy(css = ".ng-star-inserted > h1")
     private WebElement welcomeText;
-    @FindBy(css = "h2:nth-child(2)")
+    @FindBy(css = ".wrapper h2")
     private WebElement signInDetailsText;
-    @FindBy(css = "label:nth-child(1)")
+    @FindBy(css = "label[for=\"email\"]")
     private WebElement emailLabel;
     @FindBy(id = "email")
     private WebElement emailInput;
@@ -34,7 +33,7 @@ public class SignInTest {
     private WebElement passwordInput;
     @FindBy(css = ".ubsStyle")
     private WebElement signInSubmitButton;
-    @FindBy(xpath = "//*[@id=\"header_user-wrp\"]/ul/li[2] [@class=\"drop-down-item\"]")
+    @FindBy(xpath = "//*[@id=\"header_user-wrp\"]/ul/li[@aria-label=\"sign-out\"] [@class=\"drop-down-item\"]")
     private WebElement signOutButton;
     @FindBy(css = ".mat-simple-snackbar > span")
     private WebElement result;
@@ -48,8 +47,8 @@ public class SignInTest {
     private WebElement headerUser;
     @FindBy(xpath = "//app-main//app-ubs/app-header/header//ul[@id=\"header_user-wrp\"]")
     private WebElement headerUserButton;
-    @FindBy(xpath = "//*[contains(@id,\"mat-dialog-\")]/app-auth-modal//div[@class=\"right-side\"]/a")
-//    @FindBy(xpath = "//*[contains(@id,\"mat-dialog-\")]/app-auth-modal/div/div/div[2]/a")
+    //    @FindBy(xpath = "//*[contains(@id,\"mat-dialog-\")]/app-auth-modal//div[@class=\"right-side\"]/a")
+    @FindBy(css = "a.close-modal-window")
     private WebElement closeButton;
     @FindBy(xpath = "//app-auth-modal/div/div/div[2]/div/app-sign-in/form/label[2]")
     private WebElement passwordLabel;
@@ -73,7 +72,7 @@ public class SignInTest {
     private WebElement redirectSignUpText;
     @FindBy(xpath = "//*[contains(@id,\"mat-dialog-\")]/app-auth-modal//app-sign-in/div[@class=\"missing-account\"]/p/a")
     private WebElement redirectSignUpButton;
-    @FindBy(xpath = "//*/app-auth-modal//app-sign-up/h1")
+    @FindBy(css = "h1.title-text")
     private WebElement helloText;
     @FindBy(xpath = "//*/app-auth-modal//app-sign-up/h2")
     private WebElement signUpDetailsText;
@@ -85,6 +84,7 @@ public class SignInTest {
 //    private WebElement signInGoogleText;
 
     private static WebDriver driver;
+    private static WebDriverWait webDriverWait;
 
     @BeforeAll
     public static void setUp() {
@@ -92,11 +92,20 @@ public class SignInTest {
         driver = new ChromeDriver();
         driver.get("https://www.greencity.social/");
         driver.manage().window().setSize(new Dimension(1264, 798));
+//        driver.manage().window().maximize();
     }
 
     @BeforeEach
     public void initPageElements() {
         PageFactory.initElements(driver, this);
+        driver.manage().deleteAllCookies();
+        webDriverWait = new WebDriverWait(driver, Duration.ofMillis(5000L));
+        // ToDo delete token
+    }
+
+    private void clickSignInButton() {
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(signInButton));
+        signInButton.click();
     }
 
     @Test
@@ -105,14 +114,12 @@ public class SignInTest {
     }
 
     @ParameterizedTest
-    @CsvSource({
-            "magicnimfa@gmail.com, qypsaw-1hIvsy-tikcaq, SeliveystrovaElenna",
-            "testerforapp2023@gmail.com, nehmys-hipbic-5jadcU, RimaUkraine",
-            "cowafo7557@bustayes.com, zyzkug-dacxoq-2hoswY, KiraNightly"
-    })
-    public void signInTest(String email, String password, String userName) throws InterruptedException {
-        signInButton.click();
-        Thread.sleep(3000);
+    @CsvFileSource(resources = "/usersData.csv")
+    public void signInTest(String email, String password, String userName) {
+        clickSignInButton();
+
+        webDriverWait.until(ExpectedConditions.textToBePresentInElement(welcomeText, "Welcome back!"));
+
         MatcherAssert.assertThat(welcomeText.getText(), CoreMatchers.is("Welcome back!"));
         MatcherAssert.assertThat(signInDetailsText.getText(), CoreMatchers.is("Please enter your details to sign in."));
         MatcherAssert.assertThat(emailLabel.getText(), CoreMatchers.is("Email"));
@@ -122,22 +129,23 @@ public class SignInTest {
         MatcherAssert.assertThat(passwordInput.getAttribute("value"), CoreMatchers.is(password));
         signInSubmitButton.click();
 
-        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofMillis(2000L));
+//        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(2000L));
         webDriverWait.until(ExpectedConditions.elementToBeClickable(headerUser));
         MatcherAssert.assertThat(headerUser.getText(), CoreMatchers.is(userName));
         headerUserButton.click();
-        Thread.sleep(5000);
+
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(headerUser));
         headerUser.isSelected();
         signOutButton.click();
     }
 
     @ParameterizedTest
-    @CsvSource({
+    @ValueSource(strings = {
             "samplestesgreencity@gmail",
             "samplestesgreencity.com"
     })
     public void signInNotValidEmail(String email) {
-        signInButton.click();
+        clickSignInButton();
         emailInput.sendKeys(email);
         passwordInput.sendKeys("uT346^^^erw");
         MatcherAssert.assertThat(errorEmail.getText(), CoreMatchers.is("Please check if the email is written correctly"));
@@ -151,7 +159,7 @@ public class SignInTest {
             "testerforapp2023@gmail.com, 123456789123456789123456789123456789, Password must be less than 20 characters long without spaces."
     })
     public void signInNotValidPassword(String email, String password, String message) {
-        signInButton.click();
+        clickSignInButton();
         emailInput.sendKeys(email);
         passwordInput.sendKeys(password);
         passwordLabel.click();
@@ -161,71 +169,87 @@ public class SignInTest {
     }
 
     @ParameterizedTest
-    @CsvSource({"testerforapp2023@gmail.com, 12345678, Bad email or password"})
+    @CsvSource({
+            "testerforapp2023@gmail.com, 12345678, Bad email or password",
+            "testerforaApp2023@gmail.com, nehmys-hipbic-5jadcU, Bad email or password"
+    })
     public void signInIncorrectData(String email, String password, String message) throws InterruptedException {
-        signInButton.click();
+        clickSignInButton();
         emailInput.sendKeys(email);
         passwordInput.sendKeys(password);
         signInSubmitButton.click();
-        Thread.sleep(5000);
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000L));
         WebElement errorSubmit = driver.findElement(By.xpath("//*[contains(@id,\"mat-dialog-\")]/app-auth-modal//app-sign-in/form/div[@class=\"alert-general-error ng-star-inserted\"]"));
-
         MatcherAssert.assertThat(errorSubmit.getText(), CoreMatchers.is(message));
         closeButton.click();
     }
 
-    @ParameterizedTest
-    @CsvSource({
-            "magicnimfa@gmail.com",
-            "testerforapp2023@gmail.com",
-            "cowafo7557@bustayes.com",
-            "xapajoy635@frandin.com"
-    })
-    public void forgotPasswordTest(String email) throws InterruptedException {
-        signInButton.click();
+    @Test
+    public void checkFormForgotPasswordTest() {
+        clickSignInButton();
         forgotPasswordButton.click();
         MatcherAssert.assertThat(troubleText.getText(), CoreMatchers.is("Trouble singing in?"));
         MatcherAssert.assertThat(troubleDetailsText.getText(), CoreMatchers.is("Enter your email address and we'll send you a link to regain access to your account."));
-        emailInput.sendKeys(email);
-        submitLoginLinkButton.click();
-        Thread.sleep(2000);
-        MatcherAssert.assertThat(sendLinkMessage.getText(), CoreMatchers.is("Restore password link was sent to your email address."));
-
-    }
-    @ParameterizedTest
-    @CsvSource({
-            "magicnimfa@gmail.com",
-            "testerforapp2023@gmail.com",
-            "cowafo7557@bustayes.com",
-            "xapajoy635@frandin.com"
-    })
-    public void forgotPasswordLinkAlreadySent(String email) throws InterruptedException {
-        signInButton.click();
-        forgotPasswordButton.click();
-        MatcherAssert.assertThat(troubleText.getText(), CoreMatchers.is("Trouble singing in?"));
-        MatcherAssert.assertThat(troubleDetailsText.getText(), CoreMatchers.is("Enter your email address and we'll send you a link to regain access to your account."));
-        emailInput.sendKeys(email);
-        submitLoginLinkButton.click();
-        Thread.sleep(2000);
-        MatcherAssert.assertThat(errorSubmitLink.getText(), CoreMatchers.is("Password restore link already sent, please check your email: " + email));
         MatcherAssert.assertThat(backToSignInText.getText(), CoreMatchers.is("Remember your password? Back to Sign in"));
         backToSignInButton.click();
         closeButton.click();
+    }
 
+    private static Stream<String> emailProvider() {
+        return Stream.of(
+                "magicnimfa@gmail.com",
+                "testerforapp2023@gmail.com",
+                "cowafo7557@bustayes.com",
+                "xapajoy635@frandin.com"
+
+        );
+    }
+
+    private void fillForgotPasswordForm(String email) {
+        clickSignInButton();
+        forgotPasswordButton.click();
+        emailInput.sendKeys(email);
+        submitLoginLinkButton.click();
+    }
+
+//    @ParameterizedTest
+//    @MethodSource(value = "emailProvider")
+//    public void forgotPasswordTest(String email) {
+//        fillForgotPasswordForm(email);
+//        webDriverWait.until(ExpectedConditions.visibilityOf(sendLinkMessage));
+//        MatcherAssert.assertThat(sendLinkMessage.getText(), CoreMatchers.is("Restore password link was sent to your email address."));
+//        webDriverWait.until(ExpectedConditions.elementToBeClickable(closeButton));
+//        closeButton.click();
+//    }
+
+    @ParameterizedTest()
+    @MethodSource(value = "emailProvider")
+    public void forgotPasswordLinkAlreadySentTest(String email) {
+        fillForgotPasswordForm(email);
+        webDriverWait.until(ExpectedConditions.textToBePresentInElement(errorSubmitLink,
+                "Password restore link already sent, please check your email: " + email));
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(closeButton));
+        closeButton.click();
     }
 
     @Test
     public void redirectSignUp() {
-        signInButton.click();
+        clickSignInButton();
         MatcherAssert.assertThat(redirectSignUpText.getText(), CoreMatchers.is("Don't have an account yet? Sign up"));
         redirectSignUpButton.click();
-        MatcherAssert.assertThat(helloText.getText(), CoreMatchers.is("Hello!"));
-        MatcherAssert.assertThat(signUpDetailsText.getText(), CoreMatchers.is("Please enter your details to sign up."));
-        closeButton.click();
+        try {
+            webDriverWait.until(ExpectedConditions.textToBePresentInElementValue(helloText, "Hello!"));
+            MatcherAssert.assertThat(helloText.getText(), CoreMatchers.is("Hello!"));
+            MatcherAssert.assertThat(signUpDetailsText.getText(), CoreMatchers.is("Please enter your details to sign up."));
+
+        } catch (TimeoutException e){
+            closeButton.click();
+            throw new TimeoutException(e);
+        }
     }
 //    @Test
 //    public void sighInGoogle() {
-//        signInButton.click();
+//        clickSignInButton();
 //        signInGoogleButton.click();
 //        MatcherAssert.assertThat(signInGoogleText.getText(), CoreMatchers.is("Вход через аккаунт Google"));
 //    }
